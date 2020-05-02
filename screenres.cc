ffd27030755e6626edfa3b8ff95c873c51ec8e10
Euan Haahr <euanhaahrmail2@gmail.com>
2020-05-02 17:33:51 +0100

    ported to the latest version of node and updated packages to get rid of vulnerabilities

--- screenres.cc
+++ screenres.cc
@@ -12,18 +12,17 @@
 #include "headers.h"
 
 using v8::Local;
-using v8::Handle;
 using v8::Object;
 using v8::Array;
 using v8::Number;
 using v8::String;
 using v8::FunctionTemplate;
 
-void init(Handle<Object>);
+void init(Local<Object>);
 
 NAN_METHOD(set) {
   Nan::HandleScope scope;
-  int code = changeRes(info[0]->Uint32Value(), info[1]->Uint32Value());
+  int code = changeRes(Nan::To<uint32_t>(info[0]).ToChecked(), Nan::To<uint32_t>(info[1]).ToChecked());
 
   if (!code) {
     info.GetReturnValue().SetUndefined();
@@ -36,14 +35,16 @@ NAN_METHOD(get) {
   Nan::HandleScope scope;
   Resolution res = fetchRes();
   Local<Array> arr = Nan::New<Array>(2);
-  arr->Set(0, Nan::New<Number>(res.width));
-  arr->Set(1, Nan::New<Number>(res.height));
+  Nan::Set(arr, 0, Nan::New<Number>(res.width));
+  Nan::Set(arr, 1, Nan::New<Number>(res.height));
   info.GetReturnValue().Set(arr);
 }
 
 NAN_MODULE_INIT (init) {
-  Nan::Set(target, Nan::New<String>("set").ToLocalChecked(), Nan::New<FunctionTemplate>(set)->GetFunction());
-  Nan::Set(target, Nan::New<String>("get").ToLocalChecked(), Nan::New<FunctionTemplate>(get)->GetFunction());
+  //Nan::Set(target, Nan::New<String>("set").ToLocalChecked(), Nan::New<FunctionTemplate>(set)->GetFunction());
+  //Nan::Set(target, Nan::New<String>("get").ToLocalChecked(), Nan::New<FunctionTemplate>(get)->GetFunction());
+  NAN_EXPORT(target, get);
+  NAN_EXPORT(target, set);
 }
 
 NODE_MODULE(screenres, init);
